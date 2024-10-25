b256590e9f4d4e45c526d1a954b1ae140e69ef99
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2024-10-25 15:21:30 -0700

    chore: add post-install script to php composer (#84)
    * chore: add post-install script to php composer
    
    * chore: add post-install script to php composer
    
    * chore: add way to skip post-install script
    
    * tool: add statsig-php-publish action

---
+++ statsig-php/post-install.php
@@ -0,0 +1,122 @@
+<?php
+
+const OUTPUT_DIR = "resources";
+const VERSION = "0.0.1-beta.2";
+
+if (getenv('SKIP_STATSIG_POST_INSTALL') !== 'true') {
+    exit(0);
+}
+
+function get_system_info()
+{
+    $os = PHP_OS_FAMILY;
+    $arch = php_uname('m');
+
+    if ($arch === 'amd64' || $arch === 'x86_64') {
+        $arch = 'x86_64';
+    } else if ($arch === 'arm64' || $arch === 'aarch64') {
+        $arch = 'aarch64';
+    } else {
+        echo "Unsupported architecture: $arch\n";
+        exit(1);
+    }
+
+    if ($os === 'Darwin') {
+        $os = 'macos';
+    } else if ($os === 'Linux') {
+        $os = 'linux';
+    } else if ($os === 'Windows') {
+        $os = 'windows';
+    } else {
+        echo "Unsupported OS: $os\n";
+        exit(1);
+    }
+
+    echo "System Info: {$os} {$arch}\n";
+
+    return [$os, $arch];
+}
+
+function ensure_bin_dir_exists()
+{
+    if (is_dir(OUTPUT_DIR)) {
+        return;
+    }
+
+    if (!mkdir(OUTPUT_DIR, 0755, true)) {
+        echo "Failed to create directory: " . OUTPUT_DIR . "\n";
+        exit(1);
+    }
+}
+
+function download_binary($system_info)
+{
+    $binary_map = [
+        "macos-aarch64" => "statsig-ffi-aarch64-apple-darwin-release.zip",
+        "linux-aarch64" => "statsig-ffi-aarch64-unknown-linux-gnu-release.zip",
+        "linux-x86_64" => "statsig-ffi-x86_64-unknown-linux-gnu-release.zip",
+        "windows-x86_64" => "statsig-ffi-x86_64-pc-win32-msvc-release.zip",
+    ];
+
+    $binary_file = $binary_map[$system_info[0] . "-" . $system_info[1]] ?? null;
+
+    if ($binary_file === null) {
+        echo "No binary found for: {$system_info[0]} {$system_info[1]}\n";
+        exit(1);
+    }
+
+    $url = "https://github.com/statsig-io/sigstat-php/releases/download/" . VERSION . "/" . $binary_file;
+
+    echo "Downloading binary from $url\n";
+
+    $output_path = OUTPUT_DIR . "/" . $binary_file;
+
+    file_put_contents($output_path, file_get_contents($url));
+
+    return $output_path;
+}
+
+function unzip_binary($zip_file_path)
+{
+    echo "Unzipping binary\n";
+    $zip = new ZipArchive();
+    if ($zip->open($zip_file_path) === TRUE) {
+        for ($i = 0; $i < $zip->numFiles; $i++) {
+            $filename = $zip->getNameIndex($i);
+            if (in_array($filename, ['libstatsig_ffi.dylib', 'statsig_ffi.dll', 'libstatsig_ffi.so'])) {
+                $zip->extractTo(OUTPUT_DIR, $filename);
+                echo "Extracted $filename\n";
+            }
+        }
+        $zip->close();
+        echo "Binary unzipped to " . OUTPUT_DIR . "\n";
+    } else {
+        echo "Failed to open zip file\n";
+        exit(1);
+    }
+
+    echo "Binary unzipped to " . OUTPUT_DIR . "\n";
+
+    echo "Deleting zip file\n";
+    if (unlink($zip_file_path)) {
+        echo "Successfully deleted $zip_file_path\n";
+    } else {
+        echo "Failed to delete $zip_file_path\n";
+    }
+}
+
+function download_header()
+{
+    $url = "https://github.com/statsig-io/sigstat-php/releases/download/" . VERSION . "/statsig_ffi.h";
+    echo "Downloading header from $url\n";
+
+    $output_path = OUTPUT_DIR . "/statsig_ffi.h";
+    file_put_contents($output_path, file_get_contents($url));
+}
+
+
+$system_info = get_system_info();
+ensure_bin_dir_exists();
+$zip_file_path = download_binary($system_info);
+unzip_binary($zip_file_path);
+download_header();
--- statsig-php/src/StatsigFFI.php
+++ statsig-php/src/StatsigFFI.php
@@ -9,17 +9,75 @@ class StatsigFFI
     private static ?FFI $ffi = null;
     public static function get(): FFI
     {
-        if (self::$ffi === null) {
-            $dir = dirname(__FILE__);
-            self::$ffi = FFI::cdef(
-                file_get_contents($dir . '/../../statsig-ffi/include/statsig_ffi.h'),
-                match (PHP_OS_FAMILY) {
-                    'Darwin' => $dir . '/../../target/debug/libstatsig_ffi.dylib',
-                    'Windows' => $dir . '/../../target/debug/statsig_ffi.dll',
-                    default => $dir . '/../../target/debug/libstatsig_ffi.so',
-                }
-            );
+        if (self::$ffi !== null) {
+            return self::$ffi;
         }
+
+        $found_binary_path = null;
+        $found_header_path = null;
+
+        $bin = dirname(__FILE__) . '/../resources';
+        if (is_dir($bin)) {
+            $found_binary_path = self::find_binary_in_dir($bin);
+            $found_header_path = self::find_header_file_in_dir($bin);
+        }
+
+        if ($found_binary_path === null) {
+            $target_dir = dirname(__FILE__) . '/../../target/debug';
+            if (is_dir($target_dir)) {
+                $found_binary_path = self::find_binary_in_dir($target_dir);
+            }
+        }
+
+        if ($found_header_path === null) {
+            $include_dir = dirname(__FILE__) . '/../../statsig-ffi/include';
+            if (is_dir($include_dir)) {
+                $found_header_path = self::find_header_file_in_dir($include_dir);
+            }
+        }
+
+
+        if ($found_binary_path === null) {
+            throw new \Exception("Binary not found in $bin");
+        }
+
+        if ($found_header_path === null) {
+            throw new \Exception("Header file not found in $include_dir");
+        }
+
+        self::$ffi = FFI::cdef(
+            file_get_contents($found_header_path),
+            $found_binary_path
+        );
+
         return self::$ffi;
+    }
+
+    private static function find_binary_in_dir(string $dir): ?string
+    {
+        $file_name = match (PHP_OS_FAMILY) {
+            'Darwin' => 'libstatsig_ffi.dylib',
+            'Windows' => 'statsig_ffi.dll',
+            default => 'libstatsig_ffi.so',
+        };
+
+        $path = $dir . '/' . $file_name;
+
+        if (file_exists($path)) {
+            return $path;
+        }
+
+        return null;
+    }
+
+    private static function find_header_file_in_dir(string $dir): ?string
+    {
+        $path = $dir . '/statsig_ffi.h';
+
+        if (file_exists($path)) {
+            return $path;
+        }
+
+        return null;
     }
 }
