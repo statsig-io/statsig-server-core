c25e1d1091e5e50d8d4e2ffc19c81e6f6338e9ca
Daniel Loomb <daniel@statsig.com>
2024-11-06 22:03:16 -0800

    tool: allow user_access_token auth

--- exec/src/utils/octocrab_utils.rs
+++ exec/src/utils/octocrab_utils.rs
@@ -6,17 +6,26 @@ pub async fn get_octocrab() -> Octocrab {
         .expect("The OCTOCRAB_TOKEN environment variable was not found");
 
     let octo = OctocrabBuilder::new()
-        .personal_token(token)
+        .personal_token(token.clone())
         .build()
         .expect("Failed to create Octocrab instance");
 
-    if let Ok(app) = octo.current().app().await {
-        println!("Successfully authenticated as: {}", app.name.bold());
-    } else if let Ok(user) = octo.current().user().await {
-        println!("Successfully authenticated as: {}", user.login.bold());
-    } else {
-        panic!("Failed to authenticate");
+    if is_authenticated(&octo).await {
+        return octo;
     }
 
+    println!("Unknown authentication. Things may not work as expected.");
     return octo;
+}
+
+async fn is_authenticated(octo: &Octocrab) -> bool {
+    if let Ok(app) = octo.current().app().await {
+        println!("Successfully authenticated as: {}", app.name.bold());
+        true
+    } else if let Ok(user) = octo.current().user().await {
+        println!("Successfully authenticated as: {}", user.login.bold());
+        true
+    } else {
+        false
+    }
 }
