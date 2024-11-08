d5779e57be60b80ed7c267445cad4f2856cb5591
Daniel Loomb <daniel@statsig.com>
2024-11-07 16:23:12 -0800

    tool: support auth with app key

--- exec/src/utils/octocrab_utils.rs
+++ exec/src/utils/octocrab_utils.rs
@@ -1,18 +1,36 @@
 use colored::*;
-use octocrab::{Octocrab, OctocrabBuilder};
+use jsonwebtoken::EncodingKey;
+use octocrab::{models::InstallationId, Octocrab, OctocrabBuilder};
 
 pub async fn get_octocrab() -> Octocrab {
-    let token = std::env::var("OCTOCRAB_TOKEN")
-        .expect("The OCTOCRAB_TOKEN environment variable was not found");
+    if let Ok(app_key) = std::env::var("KONG_APP_PRIVATE_KEY") {
+        println!("Authenticating with app key");
+
+        let octo = get_octo_with_app_key(&app_key);
+
+        if is_authenticated(&octo).await {
+            let installation_id = InstallationId::from(36921303);
+            let octo = octo
+                .installation(installation_id)
+                .expect("Failed to get installation");
+
+            println!("Authenticated as App Installation");
+            return octo;
+        }
+    }
+
+    if let Ok(token) = std::env::var("OCTOCRAB_TOKEN") {
+        println!("Authenticating with personal token");
+
+        let octo = get_octo_with_personal_token(&token);
+        if is_authenticated(&octo).await {
+            return octo;
+        }
+    }
 
     let octo = OctocrabBuilder::new()
-        .personal_token(token.clone())
         .build()
         .expect("Failed to create Octocrab instance");
-
-    if is_authenticated(&octo).await {
-        return octo;
-    }
 
     println!("Unknown authentication. Things may not work as expected.");
     return octo;
@@ -28,4 +46,21 @@ async fn is_authenticated(octo: &Octocrab) -> bool {
     } else {
         false
     }
+}
+
+fn get_octo_with_app_key(app_key: &str) -> Octocrab {
+    let key = EncodingKey::from_rsa_pem(app_key.as_bytes()).expect("Failed to parse RSA key");
+    let app_id = octocrab::models::AppId(229901);
+
+    OctocrabBuilder::new()
+        .app(app_id, key)
+        .build()
+        .expect("Failed to create Octocrab instance")
+}
+
+fn get_octo_with_personal_token(token: &str) -> Octocrab {
+    OctocrabBuilder::new()
+        .personal_token(token)
+        .build()
+        .expect("Failed to create Octocrab instance")
 }
