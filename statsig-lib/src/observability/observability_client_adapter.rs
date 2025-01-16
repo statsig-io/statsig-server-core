25c24eafb99476a5000c673767806874416e00b2
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-01-16 12:29:19 -0800

    Add comments on the trait (#272)
    update

--- statsig-lib/src/observability/observability_client_adapter.rs
+++ statsig-lib/src/observability/observability_client_adapter.rs
@@ -40,6 +40,9 @@ pub trait ObservabilityClient: Send + Sync + 'static + OpsStatsEventObserver {
     fn increment(&self, metric_name: String, value: f64, tags: Option<HashMap<String, String>>);
     fn gauge(&self, metric_name: String, value: f64, tags: Option<HashMap<String, String>>);
     fn dist(&self, metric_name: String, value: f64, tags: Option<HashMap<String, String>>);
+    // This is needed since upper casting is not officially supported yet 
+    // (WIP to support https://github.com/rust-lang/rust/issues/65991)
+    // For implementation, just return Self; should be sufficient
     fn to_ops_stats_event_observer(self: Arc<Self>) -> Arc<dyn OpsStatsEventObserver>;
 }
 
