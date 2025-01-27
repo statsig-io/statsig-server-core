31e1ff20f63ec372d902c8a5cbf2f824e702cb7f
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-01-27 13:28:38 -0800

    chore: bump version to 0.0.3 (#311)
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-lib/src/statsig_metadata.rs
+++ statsig-lib/src/statsig_metadata.rs
@@ -33,7 +33,7 @@ pub struct StatsigMetadata {
 impl StatsigMetadata {
     fn new() -> Self {
         Self {
-            sdk_version: "0.0.2".to_string(),
+            sdk_version: "0.0.3".to_string(),
             sdk_type: "statsig-server-core".to_string(),
             session_id: Uuid::new_v4().to_string(),
             os: None,
