3295bb460796ff70f9eb100fa11b062e1496b87e
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-01-30 21:27:18 -0800

    tool: prevent publishing unless on beta or release (#354)

--- .github/build_plan.sh
+++ .github/build_plan.sh
@@ -2,17 +2,18 @@
 
 matrix_file=./.github/build_matrix.json
 
-if [ $IS_RELEASE = true ] || [ $IS_BETA = true ]; then
-    include_filter='.config'
-    exclude_filter='[]'
+should_publish=$([ $IS_NEW_RELEASE = true ])
+
+if [ $IS_MERGED_PR = true ] && [ $IS_BETA_BRANCH = true ]; then
     should_publish=true
-else
+fi
+
+if [ $should_publish = true ]; then
     include_filter='.config'
     exclude_filter='[]'
-    # Temp: build all no matter what
-    # include_filter='[.config[] | select(.arch == "x86_64")]'
-    # exclude_filter='[.config[] | select(.arch != "x86_64")]'
-    should_publish=false
+else
+    include_filter='[.config[] | select(.target == "x86_64-unknown-linux-gnu")]'
+    exclude_filter='[.config[] | select(.target != "x86_64-unknown-linux-gnu")]'
 fi
 
 included=$(jq -c "{ package, config: $include_filter }" < "$matrix_file")
@@ -21,8 +22,8 @@ excluded=$(jq -c "{ package, config: $exclude_filter }" < "$matrix_file")
 echo "build_matrix=${included}" >> $GITHUB_OUTPUT
 echo "should_publish=${should_publish}" >> $GITHUB_OUTPUT
 
-echo "Is Release: $IS_RELEASE"
-echo "Is Beta: $IS_BETA"
+echo "Is Release Branch: $IS_RELEASE_BRANCH"
+echo "Is Beta Branch: $IS_BETA_BRANCH"
 echo "Should Publish: $should_publish"
 
 echo -e "\n== Included =="
