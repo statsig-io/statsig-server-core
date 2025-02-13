ec1d9b145e7bd73f380d3c5ebd66d81982e9679b
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-02-13 10:19:32 -0800

    [release] 0.0.6-beta.3 - NAPI Improvemets (#451)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 15 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-lib/src/statsig_metadata.rs
+++ statsig-lib/src/statsig_metadata.rs
@@ -37,7 +37,7 @@ pub struct StatsigMetadata {
 impl StatsigMetadata {
     fn new() -> Self {
         Self {
-            sdk_version: "0.0.6-beta.2".to_string(),
+            sdk_version: "0.0.6-beta.3".to_string(),
             sdk_type: "statsig-server-core".to_string(),
             session_id: Uuid::new_v4().to_string(),
             os: None,
