c6f164d009cd24cd96d07f1f242da82366752521
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-04-01 18:03:43 -0700

    Fix node publisher (#716)
    fix

--- cli/src/commands/publishers/node-publisher.ts
+++ cli/src/commands/publishers/node-publisher.ts
@@ -25,6 +25,12 @@ const DIR_STRUCTURE = {
       'statsig-generated.js',
     'macos-aarch64-apple-darwin-node/**/statsig-generated.d.ts':
       'statsig-generated.d.ts',
+    'macos-aarch64-apple-darwin-node/**/error_boundary.d.ts':
+      'error-boundary.d.ts',
+    'macos-aarch64-apple-darwin-node/**/error_boundary.ts':
+      'error-boundary.ts',
+    'macos-aarch64-apple-darwin-node/**/error_boundary.js':
+      'error-boundary.js',
   },
   'aarch64-apple-darwin': {
     'macos-aarch64-apple-darwin-node/**/aarch64-apple-darwin.package.json':
