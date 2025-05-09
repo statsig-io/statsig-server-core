365bdd195da732e869e41fc9a85563dfb286bbd6
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-05-09 14:09:04 -0700

    [release] 0.3.3-beta.1 - release (#989)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 15 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-rust/src/statsig_metadata.rs
+++ statsig-rust/src/statsig_metadata.rs
@@ -49,7 +49,7 @@ pub struct StatsigMetadataWithLogEventExtras {
 impl StatsigMetadata {
     fn new() -> Self {
         Self {
-            sdk_version: "0.3.1-beta.250509".to_string(),
+            sdk_version: "0.3.3-beta.1".to_string(),
             sdk_type: "statsig-server-core".to_string(),
             session_id: Uuid::new_v4().to_string(),
             os: None,
