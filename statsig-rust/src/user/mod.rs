7f496768c68790ab1fbec535b57c0262f8a404b4
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-05-09 10:49:38 -0700

    refac: move StatsigUserBuilder to its own file (#983)
    No changes, just moved to own file

--- statsig-rust/src/user/mod.rs
+++ statsig-rust/src/user/mod.rs
@@ -1,9 +1,10 @@
 pub mod statsig_user;
+pub mod statsig_user_builder;
 pub mod statsig_user_internal;
 pub mod statsig_user_loggable;
 
 pub use statsig_user::StatsigUser;
-pub use statsig_user::StatsigUserBuilder;
+pub use statsig_user_builder::StatsigUserBuilder;
 pub use statsig_user_internal::StatsigUserInternal;
 pub use statsig_user_loggable::StatsigUserLoggable;
 pub use statsig_user_loggable::UserLoggableData;
