2402739cda134dd3ba7f639ce3a58156e5666158
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-05-13 09:10:42 -0700

    [release] 0.3.2-beta.1 - Perf improvement (#1015)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 15 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -1,7 +1,7 @@
 <?php
 
 const OUTPUT_DIR = "resources";
-const VERSION = "0.3.3-beta.250513";
+const VERSION = "0.3.2-beta.1";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
