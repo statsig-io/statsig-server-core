f09c904cef6ab2bbf5775568b237fe95f9291b8f
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-05-28 16:32:24 -0700

    chore: add core vs legacy bench for python (#1109)

---
+++ examples/python/perf-bench/python-core-bench.py
@@ -0,0 +1,136 @@
+import os
+import time
+import numpy as np
+import random
+from statsig_python_core import (
+    Statsig,
+    StatsigUser,
+    StatsigOptions,
+)
+from importlib.metadata import version
+
+sdk_version = version("statsig-python-core")
+
+statsig = Statsig(os.getenv("PERF_SDK_KEY"))
+statsig.initialize().wait()
+
+iterations = 100_000
+global_user = StatsigUser(user_id="global_user")
+
+results = {}
+
+
+def log_benchmark(name, p99):
+    print(f"{name.ljust(50)} {p99:.4f}ms")
+    statsig.log_event(
+        global_user,
+        "sdk_benchmark",
+        p99,
+        {
+            "benchmarkName": name,
+            "sdkType": "statsig-server-core-python",
+            "sdkVersion": sdk_version,
+        },
+    )
+
+
+def make_random_user():
+    return StatsigUser(user_id=f"user_{random.randint(0, 1000000)}")
+
+
+def benchmark(func):
+    durations = []
+
+    for _ in range(iterations):
+        start = time.perf_counter()
+        func()
+        end = time.perf_counter()
+        durations.append((end - start) * 1000)  # ms
+
+    return np.percentile(durations, 99)
+
+
+def run_check_gate():
+    def action():
+        user = make_random_user()
+        statsig.check_gate(user, "test_public")
+
+    p99 = benchmark(action)
+    results["check_gate"] = p99
+
+
+def run_check_gate_global_user():
+    def action():
+        statsig.check_gate(global_user, "test_public")
+
+    p99 = benchmark(action)
+    results["check_gate_global_user"] = p99
+
+
+def run_get_feature_gate():
+    def action():
+        user = make_random_user()
+        statsig.get_feature_gate(user, "test_public")
+
+    p99 = benchmark(action)
+    results["get_feature_gate"] = p99
+
+
+def run_get_feature_gate_global_user():
+    def action():
+        statsig.get_feature_gate(global_user, "test_public")
+
+    p99 = benchmark(action)
+    results["get_feature_gate_global_user"] = p99
+
+
+def run_get_experiment():
+    def action():
+        user = make_random_user()
+        statsig.get_experiment(user, "an_experiment")
+
+    p99 = benchmark(action)
+    results["get_experiment"] = p99
+
+
+def run_get_experiment_global_user():
+    def action():
+        statsig.get_experiment(global_user, "an_experiment")
+
+    p99 = benchmark(action)
+    results["get_experiment_global_user"] = p99
+
+
+def run_get_client_initialize_response():
+    def action():
+        statsig.get_client_initialize_response(global_user)
+
+    p99 = benchmark(action)
+    results["get_client_initialize_response"] = p99
+
+
+def run_get_client_initialize_response_global_user():
+    def action():
+        statsig.get_client_initialize_response(global_user)
+
+    p99 = benchmark(action)
+    results["get_client_initialize_response_global_user"] = p99
+
+
+if __name__ == "__main__":
+    print(f"Statsig Python Core (v{sdk_version})")
+    print("--------------------------------")
+    run_check_gate()
+    run_check_gate_global_user()
+    run_get_feature_gate()
+    run_get_feature_gate_global_user()
+    run_get_experiment()
+    run_get_experiment_global_user()
+    run_get_client_initialize_response()
+    run_get_client_initialize_response_global_user()
+
+    for name, p99 in results.items():
+        log_benchmark(name, p99)
+
+    statsig.shutdown().wait()
+    print("\n\n")
