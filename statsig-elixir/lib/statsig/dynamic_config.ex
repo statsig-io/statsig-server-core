c7c5d98365c0e22f522e61b658283d4909370696
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-08-18 18:02:48 -0700

    [Cherrypick][releases/0.7.4-rc.2508182310] Fix elixir publish action + elixir: add gcir, make user_id or custom_id required, and some small fix (#1646)
    * [auto][cherrypick] 04582584ed3822663c849d9e7273b294aaf4fe47 Fix elixir publish action (#1640)
    
    * [auto][cherrypick] f1097f521c94e2fbb5364f3d9d0d9fecc18f5888 elixir: add gcir, make user_id or custom_id required, and some small fix (#1633)

--- statsig-elixir/lib/statsig/dynamic_config.ex
+++ statsig-elixir/lib/statsig/dynamic_config.ex
@@ -3,14 +3,14 @@ defmodule Statsig.DynamicConfig do
     :name,
     :value,
     :rule_id,
-    :id_type
+    :id_type,
   ]
 
   @type t :: %__MODULE__{
           name: String.t(),
           value: String.t(),
           rule_id: String.t(),
-          id_type: String.t()
+          id_type: String.t(),
         }
 
   def get_param_value(config, param_name) do
