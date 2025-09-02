512799470def668289d002a91041c8a4ba82c346
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-09-02 15:01:49 -0700

    chore: bump version to 0.8.5

--- statsig-go/cmd/post-install/main.go
+++ statsig-go/cmd/post-install/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	version = "0.8.5-rc.2509011313"
+	version = "0.8.5"
 )
 
 var output_dir string
--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -1,7 +1,7 @@
 <?php
 
 const OUTPUT_DIR = "resources";
-const VERSION = "0.8.5-rc.2509011313";
+const VERSION = "0.8.5";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
