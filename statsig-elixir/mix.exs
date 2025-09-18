3a0d78f35570fefba99c4a2f858e85c151f32b65
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-09-18 12:39:20 -0700

    [release] 0.9.1-rc.2509181924 - Fix the npm node core flavors [rc] (#1929)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.9.1-rc.2509171908",
+      version: "0.9.1-rc.2509181924",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.9.1-rc.2509171908";
+const VERSION = "0.9.1-rc.2509181924";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
