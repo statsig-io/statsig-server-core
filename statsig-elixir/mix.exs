d2296024b6e3c90cc55f3215a7d8cef515463bbe
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-10-24 17:13:13 -0700

    [release] 0.10.3-rc.6 - Support passing down undefined when getConfig, fix for idLists [rc] (#2176)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 17 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.10.3-rc.2510210409",
+      version: "0.10.3-rc.6",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
