22c0d1b3f4a13dfc98dff033e632ce9904ac2785
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-10-27 11:51:22 -0700

    [release] 0.11.0 - ⚠️Elixir DynamicConfig and Experiment API change, Node Core Improvement, ID Lists fixes [rc] (#2183)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 17 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.10.3-rc.6",
+      version: "0.11.0",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
