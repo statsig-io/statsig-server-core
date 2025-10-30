226c6e0f8a72f1a1b6dee7e73bd41a729613da55
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-10-30 15:57:15 -0700

    [release] 0.11.1 - Expose StatsigEnvironment On User Object, PHP Fixes [rc] (#2217)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 17 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.11.1-rc.2510292219";
+const VERSION = "0.11.1";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
