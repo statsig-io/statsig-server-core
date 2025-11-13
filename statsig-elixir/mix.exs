3fa5465d230bdd4b759d913d7bf4c2831f71697b
Xin Li <137219293+xinlili-statsig@users.noreply.github.com>
2025-11-12 16:35:12 -0800

    [release] 0.12.1-rc.2511121816 - Revert python typing change [rc] (#2266)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 17 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.12.1-rc.2511110013",
+      version: "0.12.1-rc.2511121816",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.12.1-rc.2511110013";
+const VERSION = "0.12.1-rc.2511121816";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
