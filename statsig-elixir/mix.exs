f3d435d62f2c920138e225db317953085b96a695
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-11-17 11:42:02 -0800

    [release] 0.12.1 - Proxy Config in Php Core, Beta Release for CPP [rc] (#2298)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.12.1-rc.2511121816",
+      version: "0.12.1",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
