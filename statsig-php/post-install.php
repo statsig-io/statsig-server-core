d29d50c5a6b2459ea25a96446b6dbac9226ba62d
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-12-01 14:33:38 -0800

    [release] 0.13.0 - Add support for protobuf, new Layer evaluation, node console log capture [rc] (#2397)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.12.2-rc.2511250016";
+const VERSION = "0.13.0";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
--- statsig-rust/src/statsig_metadata.rs
+++ statsig-rust/src/statsig_metadata.rs
@@ -49,7 +49,7 @@ pub struct StatsigMetadataWithLogEventExtras {
 impl StatsigMetadata {
     fn new() -> Self {
         Self {
-            sdk_version: "0.12.2-rc.2511250016".to_string(),
+            sdk_version: "0.13.0".to_string(),
             sdk_type: "statsig-server-core".to_string(),
             session_id: Uuid::new_v4().to_string(),
             os: None,
