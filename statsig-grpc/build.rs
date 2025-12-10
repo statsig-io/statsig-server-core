d9152b2e945ca0542f5a023f014bfe68c11fdc15
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2025-12-10 14:49:16 -0800

    tool(rust-pub): point tonic build to the correct schema file (#2448)

--- statsig-grpc/build.rs
+++ statsig-grpc/build.rs
@@ -1,4 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("../api-interface-definitions/protos/statsig_forward_proxy.proto")?;
+    // `cargo package` needs the proto to be in the src directory
+    if std::env::var("STATSIG_PUBLISH_RUST").is_ok() {
+        tonic_build::compile_protos("src/protos/statsig_forward_proxy.proto")?;
+    } else {
+        tonic_build::compile_protos(
+            "../api-interface-definitions/protos/statsig_forward_proxy.proto",
+        )?;
+    }
     Ok(())
 }
--- statsig-rust/build.rs
+++ statsig-rust/build.rs
@@ -1,4 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // `cargo package` needs the proto to be in the src directory
+    if std::env::var("STATSIG_PUBLISH_RUST").is_ok() {
+        tonic_prost_build::Config::new()
+            .out_dir("src/specs_response")
+            .compile_protos(&["src/protos/config_specs.proto"], &["src/protos"])?;
+
+        return Ok(());
+    }
+
     tonic_prost_build::Config::new()
         .out_dir("src/specs_response")
         .compile_protos(
