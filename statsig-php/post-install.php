d9567ade1a7c727a0374d0fece1e498e2c27a5ae
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-12-15 15:53:00 -0800

    Revert "[automated] chore: bump version to 0.14.0-rc.1" (#2483)
    Revert "[automated] chore: bump version to 0.14.0-rc.1 (#2481)"
    
    This reverts commit cafeb7b00a6298aee08b3c01a491e94fef9576b9.

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.14.0-rc.1";
+const VERSION = "0.13.1-beta.2512140246";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
