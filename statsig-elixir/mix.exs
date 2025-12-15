d37726274e291f86d54103726a24e754f2f3f12c
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-12-15 15:57:37 -0800

    Revert "[release] 0.14.0 - Protobuf Support, Node Core Dynamic Config Perf Enhancement [rc]" (#2484)
    Revert "[release] 0.14.0 - Protobuf Support, Node Core Dynamic Config Perf En…"
    
    This reverts commit d39eeccc73ac95ecb3bde979957cfa28a44e5f27.

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.14.0",
+      version: "0.13.1-rc.2512092052",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
