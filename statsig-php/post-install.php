e0e64e5aef51cf5504e7dee116ba37b9083c9845
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2025-12-18 19:27:56 -0800

    [release] 0.14.1-rc.2512190311 - Node Core LogEvent Support [rc] (#2512)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.14.1-rc.2512162207";
+const VERSION = "0.14.1-rc.2512190311";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
