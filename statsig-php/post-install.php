bdf6fbb0850697e4957149647a47b971cc4499a1
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-01-22 17:53:18 -0800

    [release] 0.14.2-rc.2601230113 - Test Logging [rc] (#2576)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.14.2-rc.2601200135";
+const VERSION = "0.14.2-rc.2601230113";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
--- statsig-rust/src/statsig_metadata.rs
+++ statsig-rust/src/statsig_metadata.rs
@@ -10,7 +10,7 @@ lazy_static! {
     static ref STATSIG_METADATA: RwLock<StatsigMetadata> = RwLock::new(StatsigMetadata::new());
 }
 
-pub const SDK_VERSION: &str = "0.14.2-rc.2601200135";
+pub const SDK_VERSION: &str = "0.14.2-rc.2601230113";
 
 const TAG: &str = stringify!(StatsigMetadata);
 
