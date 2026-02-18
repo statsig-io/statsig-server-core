b13d1be1ff6a136a8a14b34aed3ae68aa7372f26
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-02-17 19:23:52 -0800

    [release] 0.16.0 - Observability Client Metric Change, GCIR Improvements. [rc] (#2677)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.15.2-rc.2602172253";
+const VERSION = "0.16.0";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
