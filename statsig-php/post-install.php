3eafb956d5423341a94d4b431a645458054b8974
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-02-19 14:59:30 -0800

    [release] 0.16.1 - More OB Metrics [rc] (#2684)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 17 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.16.1-rc.2602180405";
+const VERSION = "0.16.1";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
