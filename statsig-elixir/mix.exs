b9ccc8dcbdcd176e41f4402374fb45db6335a0cd
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-02-20 14:15:46 -0800

    [release] 0.16.2-rc.2602202152 - CheckSum fix [rc] (#2693)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.16.2-rc.2602200040",
+      version: "0.16.2-rc.2602202152",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.16.2-rc.2602200040";
+const VERSION = "0.16.2-rc.2602202152";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
