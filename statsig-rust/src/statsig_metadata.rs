b67429c4ec0a096bd23775513c6dccec06130810
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-02-23 16:29:48 -0800

    [release] 0.16.2 - Delta support [rc] (#2698)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 17 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-rust/src/statsig_metadata.rs
+++ statsig-rust/src/statsig_metadata.rs
@@ -10,7 +10,7 @@ lazy_static! {
     static ref STATSIG_METADATA: RwLock<StatsigMetadata> = RwLock::new(StatsigMetadata::new());
 }
 
-pub const SDK_VERSION: &str = "0.16.2-rc.2602202152";
+pub const SDK_VERSION: &str = "0.16.2";
 
 const TAG: &str = stringify!(StatsigMetadata);
 
