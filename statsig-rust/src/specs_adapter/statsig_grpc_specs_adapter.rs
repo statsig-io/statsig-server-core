8475383067bc3d0978857614fd9d37cbd5331196
brent-statsig <126510059+brent-statsig@users.noreply.github.com>
2026-02-25 19:29:18 -0800

    Update logging to include success along with failures (#2709)
    Realized having failures without context of success is kind of meaningless to understand how bad things are. Updated logging and cleaned up

--- statsig-rust/src/specs_adapter/statsig_grpc_specs_adapter.rs
+++ statsig-rust/src/specs_adapter/statsig_grpc_specs_adapter.rs
@@ -381,11 +381,7 @@ impl StatsigGrpcSpecsAdapter {
                 source_api: None,
             };
 
-            let result = listener.did_receive_specs_update(update);
-            if matches!(&result, Err(StatsigErr::ChecksumFailure(_))) {
-                self.ops_stats.log_checksum_miss_count("grpc");
-            }
-            result
+            listener.did_receive_specs_update(update)
         } else {
             Err(StatsigErr::UnstartedAdapter("Listener not set".to_string()))
         }
