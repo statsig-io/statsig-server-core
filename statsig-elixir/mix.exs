cdc6c1dba59be8edf57698c7e1328e3afa25c5c8
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-02-26 19:25:29 -0800

    [release] 0.16.3-rc.2602270324 - Delta Metrics [rc] (#2718)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.16.3-rc.2602240137",
+      version: "0.16.3-rc.2602270324",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
