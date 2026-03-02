bc34753f1ef9756a04957924841e010bb9bf9acd
Daniel <95646168+daniel-statsig@users.noreply.github.com>
2026-03-02 12:34:28 -0800

    tool: avoid adding duplicative "automated" to title (#2727)

--- cli/src/commands/merge-to-main.ts
+++ cli/src/commands/merge-to-main.ts
@@ -1,51 +1,55 @@
-import { getCurrentBranchName, mergeToMainAndPush } from '@/utils/git_utils.js';
+import { getCurrentBranchName } from "@/utils/git_utils.js";
 import {
   createPullRequestAgainstMain,
   getOctokit,
   mergePullRequest,
-} from '@/utils/octokit_utils.js';
-import { Log } from '@/utils/terminal_utils.js';
-import { execSync } from 'child_process';
+} from "@/utils/octokit_utils.js";
+import { Log } from "@/utils/terminal_utils.js";
+import { execSync } from "child_process";
 
-import { CommandBase } from './command_base.js';
+import { CommandBase } from "./command_base.js";
 
 export class MergeToMain extends CommandBase {
   constructor() {
     super(import.meta.url, {
-      description: 'Merges the current branch to main',
+      description: "Merges the current branch to main",
     });
   }
 
   override async run() {
-    Log.title('Merging to Main');
+    Log.title("Merging to Main");
 
     const octokit = await getOctokit();
-    const title = execSync('git log -1 --pretty=%B').toString().trim();
+    const title = execSync("git log -1 --pretty=%B").toString().trim();
     const branch = await getCurrentBranchName();
 
+    const finalTitle = title.includes("[automated]")
+      ? title
+      : `[automated] ${title}`;
+
     Log.stepBegin(`Creating pull request against main`);
-    Log.stepProgress(`Title: ${title}`);
+    Log.stepProgress(`Title: ${finalTitle}`);
     Log.stepProgress(`Branch: ${branch}`);
 
     const pullRequest = await createPullRequestAgainstMain(octokit, {
-      repository: 'private-statsig-server-core',
-      title: `[automated] ${title}`,
-      body: 'Created and merged automatically by T.O.R.E',
+      repository: "private-statsig-server-core",
+      title: finalTitle,
+      body: "Created and merged automatically by T.O.R.E",
       head: branch,
     });
 
-    Log.stepEnd(`Created pull request ${pullRequest.html_url}`, 'success');
+    Log.stepEnd(`Created pull request ${pullRequest.html_url}`, "success");
 
     Log.stepBegin(`Merging pull request`);
     Log.stepProgress(`Pull request number: ${pullRequest.number}`);
 
     const mergeResult = await mergePullRequest(
       octokit,
-      'private-statsig-server-core',
+      "private-statsig-server-core",
       pullRequest.number,
     );
 
     Log.stepProgress(`Merge result: ${mergeResult.message}`);
-    Log.stepEnd(`Merged pull request ${pullRequest.html_url}`, 'success');
+    Log.stepEnd(`Merged pull request ${pullRequest.html_url}`, "success");
   }
 }
