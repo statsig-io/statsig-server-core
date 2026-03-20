527339f7069f5f44ba0d82789f9eeab77016bd74
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-03-20 13:05:15 -0700

    [release] 0.17.1-rc.2603201903 - Pyo3 Fixes [rc] (#2814)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.17.1-rc.2603192322",
+      version: "0.17.1-rc.2603201903",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
