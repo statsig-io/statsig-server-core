a0f94b3ba4352fbb85600e3d285bb0b5cc81e58d
Weihao Ding <158090588+weihao-statsig@users.noreply.github.com>
2026-04-09 14:56:39 -0700

    [release] 0.19.0 - Fast Core APIs in Node and Python, Fix Node Regression [rc] (#2898)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.18.2-rc.2604080017";
+const VERSION = "0.19.0";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
