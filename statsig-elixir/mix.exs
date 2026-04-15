901708c9259167ceab07827b6fff6a84180f82f9
Daniel Loomb <loomb@openai.com>
2026-04-15 15:19:37 -0700

    chore: sync version 0.19.2

--- statsig-elixir/mix.exs
+++ statsig-elixir/mix.exs
@@ -4,7 +4,7 @@ defmodule Statsigelixir.MixProject do
   def project do
     [
       app: :statsig_elixir,
-      version: "0.19.2-rc.2604152157",
+      version: "0.19.2",
       elixir: "~> 1.0",
       start_permanent: Mix.env() == :prod,
       description: description(),
