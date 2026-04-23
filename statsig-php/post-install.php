e804e14a980b492f808ceec55166e7f6a7c59d6d
tvh-statsig <100615502+tvh-statsig@users.noreply.github.com>
2026-04-23 16:09:44 -0700

    [release] 0.19.3 - Fix Floating Point in Protobufs [rc] (#2947)
    * Bot: Updated File Cargo.toml
    
    * Bot: Version synchronized in 18 files
    
    ---------
    
    Co-authored-by: statsig-kong[bot] <statsig-kong[bot]@users.noreply.github.com>

--- statsig-php/post-install.php
+++ statsig-php/post-install.php
@@ -2,7 +2,7 @@
 
 const OUTPUT_DIR = "resources";
 const DOMAIN = "pubkey.statsig.com";
-const VERSION = "0.19.2-rc.2604152157";
+const VERSION = "0.19.3";
 
 if (getenv('SKIP_STATSIG_POST_INSTALL') === 'true') {
     exit(0);
